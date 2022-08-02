8bbba11ad1464b891d42408dd49d9895b8bfeb0d
syrmagmo <lincolnrichard2@gmail.com>
2022-08-01 23:58:49 -0300

    Create strings.py

---
+++ codigos/Modulo2/exercicios/strings.py
@@ -0,0 +1,15 @@
+"""
+    str - string
+
+"""
+
+print('Esse é meu "aspas aqui" (str).')  #Correto
+print("Esse é meu 'aspas aqui' (str).")  #Correto
+
+# print("Esse é meu "aspas aqui" (str).")  #Erro pois não podera ser execultado devido a utilização do mesmo
+# print('Esse é meu 'aspas aqui' (str).')  #Erro pois não podera ser execultado devido a utilização do mesmo
+
+print("Esse é meu \"aspas aqui\" (str).")  #Como resolver, o \ serve para "ignorar" o caracter á seguir
+print('Esse é meu \'aspas aqui\' (str).')  #Como resolver
+
+print(r'Esse é meu \'aspas aqui\' (str).')  #o R serve para interpretar tudo que estiver escrito dentro da String
