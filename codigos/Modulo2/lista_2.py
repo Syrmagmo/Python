e0816f108b8c4edf60660b8d308e4db5f7370151
syrmagmo <lincolnrichard2@gmail.com>
2022-08-26 01:17:20 -0300

    forca em python

--- codigos/Modulo2/lista_2.py
+++ codigos/Modulo2/lista_2.py
@@ -5,5 +5,36 @@ append, insert, pop, del, clear, extend, +
 min, max
 range
 """
+from unicodedata import digit
 
-#Começaremos em breve
+
+l2 = [1,2,3,4,5,6,7,8,9]
+l2.insert(0,'Banana')
+print(l2)
+del(l2[0])
+print(l2)
+del(l2[3:5])  #Para deletar os indices que você escolher
+print(l2)
+ 
+# MAX MIM
+
+print(max(l2))
+print(min(l2))
+
+# RANGE
+
+l3 = list(range(1,10))  #Devemos converter para lista
+print(l3)
+
+soma = 0
+for valor in l3:
+    soma += valor
+    print(f'{soma}+{valor}={soma}')
+print(soma)
+
+#
+l4 = ['String', True, 10, -20.5]
+
+for elemento in l4:
+    print(f"O tipo de {elemento} é {type(elemento)}")
+    
---
+++ codigos/Modulo2/lista_forca.py
@@ -0,0 +1,44 @@
+#Mini-game, Forca
+
+secreto = 'perfume'
+digitadas = []
+chances = 3 
+
+while True:
+    
+    if chances <= 0:
+        print('Você perdeu!!!')
+        break
+    
+    letra = input("Digite uma letra: ")
+    
+    if len(letra) > 1:
+        print("Ahhh isso não vale, Digite apenas uma letra.")
+        continue
+    
+    digitadas.append(letra)  #Para armazenar dados digitados pelo usuario
+    
+    if letra in secreto:
+        print(f'Uhullllll, a letra "{letra}" existe na palavra secreta.')
+    else:
+        print(f"AFFFZZ: a letra '{letra}' Não existe na palavra secreta.")
+        digitadas.pop()  #para apagar o ultimo elemento
+    
+    secreto_temporario = ''
+    for letra_secreta in secreto:
+        if letra_secreta in digitadas:
+            secreto_temporario += letra_secreta
+        else:
+            secreto_temporario += '*'  #Para exibir os *** no lugar das letraras ocultas
+  
+    if secreto_temporario == secreto:
+        print(f'Que legal, você ganhou!!! A palavra era {secreto_temporario}')
+        break
+    else:
+        print(f"A palavra secreta está assim: {secreto_temporario}")
+    
+    if letra not in secreto:
+        chances -= 1
+    
+    print(f"Você ainda tem {chances} chances.")
+    print()
